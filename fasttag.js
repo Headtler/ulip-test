cd51a62954c4bc87ae864f08a7d215359fa84f3a
Headtler <143396952+Headtler@users.noreply.github.com>
2024-06-12 19:30:33 +0530

    update

---
+++ fasttag.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const config = {
+  method: 'post',
+  url: 'https://www.ulip.dipit.gov.in/ulip/v1.0.0/OFIASTAG/01',
+  headers: { 
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJNLVdTdGF2TklJLHJlY29yZCBVbGlQIiwiaXNzIjoiTmV0bHlJbXByb3ZlIiwiaWF0IjoxNjM5ODI3OTQ1LCJleHAiOjE2Mzk4MzE1NDUsImNsaWVudF9pZCI6IjEiLCJ1c2VyX2lkIjoxfQ.Sl6J2OMDY3MaTouoqH1b4wGlrNzWkk5jZvAucp22I4E', 
+    'Content-Type': 'application/json'
+  },
+  data : JSON.stringify({
+    "vehiclenumber": "MH12PN6051"
+  })
+};
+
+axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
