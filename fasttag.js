097826c7bfffb49dabafe608636c4e34f4b4659e
Headtler <143396952+Headtler@users.noreply.github.com>
2024-07-11 10:56:17 +0530

    f

--- fasttag.js
+++ fasttag.js
@@ -40,9 +40,9 @@ async function fetchVehicleDetails(accessToken, vehicleNumber) {
   
   // Example usage
   async function main() {
-    // const vehicleNumber = 'MH18BG6198'; // Replace with actual vehicle registration number
+    const vehicleNumber = 'MH18BG6198'; // Replace with actual vehicle registration number
     // const vehicleNumber = 'MH04LE5221'; // Replace with actual vehicle registration number
-    const vehicleNumber = 'CG07BC9186'; // Replace with actual vehicle registration number
+    // const vehicleNumber = 'CG07BC9186'; // Replace with actual vehicle registration number
     // const vehicleNumber = 'MH04LE5221'; // Replace with actual vehicle registration number
     const accessToken = await getAccessToken();
     console.log(accessToken,"accessTokenaccessTokenaccessToken")
