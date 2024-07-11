7b29a2e124089ea54c20da5decfa6bce8c9762cd
Headtler <143396952+Headtler@users.noreply.github.com>
2024-07-11 10:56:03 +0530

    ulip prod test

--- fasttag.js
+++ fasttag.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 // Function to log in and return the access token
 async function getAccessToken() {
   try {
-    const response = await axios.post('https://www.ulipstaging.dpiit.gov.in/ulip/v1.0.0/user/login', {
+    const response = await axios.post('https://www.ulip.dpiit.gov.in/ulip/v1.0.0/user/login', {
       username: 'easemy_usr',  // Replace 'xxxx' with actual username
-      password: 'easemy@12062024'  // Replace 'xxxx@123' with actual password
+      password: 'easemy@01072024'  // Replace 'xxxx@123' with actual password
     }, {
       headers: {
         'Accept': 'application/json',
@@ -22,7 +22,7 @@ async function getAccessToken() {
 // Function to fetch vehicle details using the access token
 async function fetchVehicleDetails(accessToken, vehicleNumber) {
     try {
-      const response = await axios.post('https://www.ulipstaging.dpiit.gov.in/ulip/v1.0.0/FASTAG/01', {
+      const response = await axios.post('https://www.ulip.dpiit.gov.in/ulip/v1.0.0/FASTAG/01', {
         vehiclenumber: vehicleNumber
       }, {
         headers: {
